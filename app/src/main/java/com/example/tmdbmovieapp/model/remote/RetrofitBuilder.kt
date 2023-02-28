14f09f7ee823e86536dc1718ad5ae9b6e52a48e3
1+1=2 <hau.chan@jessup.edu>
2023-02-28 07:51:24 -0800

    Setup Retrofit
    - Latest Movies now can successfully request items from the API. And the result does get processed.
    (Bug is databinding doens't work well and the text is now showing.)

--- app/src/main/java/com/example/tmdbmovieapp/model/remote/RetrofitBuilder.kt
+++ app/src/main/java/com/example/tmdbmovieapp/model/remote/RetrofitBuilder.kt
@@ -38,7 +38,7 @@ object RetrofitBuilder {
         return retrofit
     }
 
-    val instanceLatestMovie by lazy {
+    val instanceLatestMovie: ApiLatestMovie by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
--- app/src/main/java/com/example/tmdbmovieapp/viewmodel/MovieListViewModel.kt
+++ app/src/main/java/com/example/tmdbmovieapp/viewmodel/MovieListViewModel.kt
@@ -1,8 +1,27 @@
 package com.example.tmdbmovieapp.viewmodel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.tmdbmovieapp.model.local.AppDatabase
+import com.example.tmdbmovieapp.model.remote.data.latestmovie.LatestMovieResponse
+import com.example.tmdbmovieapp.model.repository.LocalRepository
+import com.example.tmdbmovieapp.model.repository.RemoteRepository
+import com.example.tmdbmovieapp.model.repository.Repository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MovieListViewModel : ViewModel() {
+class MovieListViewModel(
+    application: Application,
+    private val db: AppDatabase = AppDatabase.getInstance(application),
+    private val localRepository: LocalRepository = LocalRepository(db),
+    private val remoteRepository: RemoteRepository = RemoteRepository(),
+    private val repository: Repository = Repository(localRepository, remoteRepository)
+) : AndroidViewModel(application) {
+    private val _latestMovie = MutableLiveData<LatestMovieResponse>()
+    val latestMovie: LiveData<LatestMovieResponse> get() = _latestMovie
 
     fun getTopRatedMovies() {
 
@@ -17,6 +36,13 @@ class MovieListViewModel : ViewModel() {
     }
 
     fun getLatestMovies() {
+        repository.getLatestMovie().enqueue(object : Callback<LatestMovieResponse>{
+            override fun onResponse(
+                call: Call<LatestMovieResponse>,
+                response: Response<LatestMovieResponse>
+            ) { response.body()?.let { _latestMovie.value = it } }
 
+            override fun onFailure(call: Call<LatestMovieResponse>, t: Throwable) {}
+        })
     }
 }
