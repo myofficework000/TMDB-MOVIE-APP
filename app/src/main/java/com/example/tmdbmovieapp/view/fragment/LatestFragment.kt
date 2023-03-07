3d934f71fb3aae26d3c5c48a16df6badd7b0d40f
1+1=2 <hau.chan@jessup.edu>
2023-03-07 13:19:51 -0800

    Fixed merging issues
    Ur welcome.

--- app/src/main/java/com/example/tmdbmovieapp/view/fragment/LatestFragment.kt
+++ app/src/main/java/com/example/tmdbmovieapp/view/fragment/LatestFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModelProvider
 import com.example.tmdbmovieapp.R
 import com.example.tmdbmovieapp.databinding.FragmentLatestBinding
@@ -13,8 +14,6 @@ import com.example.tmdbmovieapp.viewmodel.MovieListViewModel
 import com.example.tmdbmovieapp.viewmodel.createFactory
 
 class LatestFragment : Fragment() {
-   private lateinit var binding: FragmentLatestBinding
-
     private val viewModel by lazy {
         requireActivity().run {
             ViewModelProvider(
@@ -26,10 +25,11 @@ class LatestFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View{
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_latest, container, false)
-        return binding.root
-    }
+    ): View = DataBindingUtil.inflate<FragmentLatestBinding>(
+        inflater, R.layout.fragment_latest, container, false
+    ).apply {
+        vm = viewModel
+    }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -46,12 +46,5 @@ class LatestFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         viewModel.getLatestMovies()
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        viewModel.latestMovie.observe(this.viewLifecycleOwner){
-            binding.data = it
-        }
     }
 }
