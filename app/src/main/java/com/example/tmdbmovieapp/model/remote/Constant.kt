c6ea1c0a4c4839ca41dca62969ea9ee94849311c
cheetahmail007 <cheetahmail007@gmail.com>
2023-03-15 01:31:57 +0530

    added detail screen UI

--- app/src/main/java/com/example/tmdbmovieapp/model/remote/Constant.kt
+++ app/src/main/java/com/example/tmdbmovieapp/model/remote/Constant.kt
@@ -7,6 +7,7 @@ object Constant {
     const val TOKEN_MASTER = "a53a3bcb8e2f8efe6d2988a34d73e750"
     const val TOKEN_JOSH = "441f9a2f476e2d801a859a9955fe6188"
     const val TOKEN_ALEX = "a51ff8a6730be4f36331b0b345e9e775"
+    const val RANDOM_API_KEY = "ed75ba81d3a8253393684108406b8e26"
     const val TOKEN_THOMAS = ""
     const val TOKEN_LUAN = ""
     const val END_POINT_LATEST_MOVIES = "movie/latest"
@@ -15,11 +16,17 @@ object Constant {
     const val END_POINT_TOP_RATED_MOVIES = "movie/top_rated"
     const val END_POINT_MOVIE_DETAIL = "movie/{movie_id}"
     const val END_POINT_MOVIE_DETAIL_ARG_1 = "movie_id"
-    const val END_POINT_SEARCH ="search/movie"
+    const val END_POINT_SEARCH = "search/movie"
     const val TABLE_MOVIE = "movie"
     const val TABLE_MOVIE_DETAIL = "movieDetail"
     const val TABLE_MOVIE_GENRE = "movieGenre"
     const val TABLE_PRODUCTION_COMPANY = "productionCompany"
     const val TABLE_PRODUCTION_COUNTRY = "productionCountry"
     const val TABLE_SPOKEN_LANGUAGE = "spokenLanguage"
+
+    // for movie detail
+    //https://api.themoviedb.org/3/movie/677179?append_to_response=credits&api_key=ed75ba81d3a8253393684108406b8e26
+
+    // for now playing movie in threater
+    // https://api.themoviedb.org/3/movie/now_playing?api_key=ed75ba81d3a8253393684108406b8e26&page=12
 }
